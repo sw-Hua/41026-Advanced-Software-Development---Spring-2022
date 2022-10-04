993030241c5a1e1f019d1dcbf80fc196db01dd8e
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-10-05 00:23:05 +1100

    Delete ASDBackened directory

--- ASDBackened/src/main/java/com/asd/backened/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.asd.backened.service;
-
-import com.asd.backened.entity.Employee;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface EmployeeService extends IService<Employee> {
-}
