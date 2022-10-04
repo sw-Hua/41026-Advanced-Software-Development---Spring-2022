003bf25bc5d1f7fa315506bb8a8873ab6b54ed12
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-10-05 00:22:35 +1100

    Delete order_Management directory

--- order_Management/src/main/java/com/jingsong/assignment1/order/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jingsong.assignment1.order;
-
-public class OrderNotFoundException extends Throwable {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
--- order_Management/src/main/java/com/jingsong/assignment1/order/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jingsong.assignment1.order;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-
-    //public Long countByID(Integer id);
-}
