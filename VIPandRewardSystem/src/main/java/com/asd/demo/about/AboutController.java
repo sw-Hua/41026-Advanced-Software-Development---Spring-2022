0486ed80efc26066e0b0ead5c379cca553d450ca
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-10-07 19:35:34 +1100

    Update AboutController.java

--- VIPandRewardSystem/src/main/java/com/asd/demo/about/AboutController.java
+++ VIPandRewardSystem/src/main/java/com/asd/demo/about/AboutController.java
@@ -4,14 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.util.StreamUtils;
+
+
 import java.util.List;
 
 @Controller
@@ -19,7 +22,8 @@ public class AboutController {
     @Autowired private AboutService service;
 
     @GetMapping("/about")
-    public String showAbout(Model model){
+    public String showAbout(Model model, HttpSession session){
+        session.setAttribute("userId", 10001);
         List<About> listAbout = service.listAll();
         model.addAttribute("listAbout", listAbout);
         return "about";
